e6a648444953460ce5212714ae3e3b6d5a40a955
Kendrix0 <83741031+Kendrix0@users.noreply.github.com>
2025-01-13 19:00:47 -0600

    move mouseover event to container from cells

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const gridSize = document.querySelector('#gridSize');
 const gridColor = document.querySelector('#gridColor');
 const sizeLabel = document.querySelector('#sizeLabel');
 
+container.addEventListener('mouseover', (e) => {
+  if (e.ctrlKey) {
+    e.target.setAttribute('style', `background-color: ${gridColor.value}`)
+  }
+})
+
 function makeRows(size) {
   container.style.setProperty('--grid-rows', size);
   container.style.setProperty('--grid-cols', size);
   for (c = 0; c < (size**2); c++) {
     let cell = document.createElement('div');
     cell.classList.add('grid-item');
-    cell.addEventListener('mouseover', (e) => {
-      if (e.ctrlKey) {
-      cell.setAttribute('style', `background-color: ${gridColor.value}`)
-      }
-    });
     container.appendChild(cell);
   };
   gridSize.value = size
